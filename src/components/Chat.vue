f6ff6fe33b498cbfa2f11fad2877064d2e10fe53
Shittu-Moshood <shittumoshoodademola@gmail.com>
2022-04-05 17:59:48 +0100

    m

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="mt-10">
+  <div class="mt-5">
 
    <div v-if="isLogin" class="login">
         <div style="" class="mobile">
          <Avatar :src="user.photoURL"  />
-        <h1 class="text-center text-2xl "><strong>Meet</strong>UP</h1>
+        <h1 class="text-center text-2xl "><strong>MeetUP</strong></h1>
          
           <button class="text-gray-400 hover:text-white button" @click="Signout">
           Sign Out
@@ -18,9 +18,11 @@
         Sign in
       </button>
       <!-- <img src="../assets/img.gif" alt="" style="height: 40px; width:40px;"> -->
+<Scroll/>
+
     </div>
 
-    <Scroll/>
+    
 
     
   <!-- </div> -->
@@ -68,7 +70,7 @@ export default {
       messages,
       () => {
         nextTick(() => {
-          bottom.value.scrollIntoView({ behavior: 'smooth', block: "end", inline: "nearest" })
+          bottom.value.scrollIntoView({ behavior: 'smooth',})
         })
       },
       { deep: false }
@@ -97,12 +99,16 @@ export default {
 
 *{
   background: url('https://res.cloudinary.com/mindset/image/upload/v1648979740/75901-background_w2ni6g.gif');
- 
+  
 }
 
+.icon{
+  width: 50px;
+  height: 10px;
+}
 form{
   display: flex;
-  justify-content: center;
+  padding: 0 2em;
   align-items: center;
   
   
@@ -115,10 +121,6 @@ form{
     padding-left: 10px;
    
   }
-  .now{
-      padding-left: 10em;
-      padding-right: 10em;
-  }
 
   .button{
     width: 118px;
@@ -127,7 +129,16 @@ form{
     box-shadow: 1px 3px 4px rgba(98, 26, 123, 0.18);
     border-radius: 10px;
     color: #fff;
+    
   
+  }
+   .mobile{
+      display:flex;
+       align-items:center;
+        justify-content:space-between;
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-bottom: 10px;
   }
  
 
@@ -161,10 +172,10 @@ form{
   .bottom{
     border-bottom: solid 2px #621A7B;
     border-radius: 10px;
-    position: fixed;
+    position: absolute;
     bottom: 0;
     height: 100px;
-    overflow: hidden;
+    overflow: scroll;
   
 
   }
